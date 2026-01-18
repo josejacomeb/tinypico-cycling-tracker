11772c1b4730f3a56c39d0d6f8f25ea09fde5446
José Jácome <josejacomeb@gmail.com>
2026-01-18 12:49:52 -0500

    chore: pass the DT string to the script

--- code/test_sensor_fusion.sh
+++ code/test_sensor_fusion.sh
@@ -24,6 +24,6 @@ input_basename=$(basename "$input_csv" .csv)
 # 5. Apply sensor fusion using the UKF implementation
 python3 test_ukf_py/sensor_fusion.py --input_csv "$input_csv" --output_csv "${input_basename}_output.csv"
 # 6. Convert the output CSV to GPX format
-python3 utils/csv_to_gpx.py --input_csv "${input_basename}_output.csv" --output_gpx "${input_basename}_output.gpx"
+python3 utils/csv_to_gpx.py --input_csv "${input_basename}_output.csv" --output_gpx "${input_basename}_output.gpx" --date_time_gpx ${input_basename}
 # 6.1 Convert original data sensor data to GPX format for comparison
-python3 utils/csv_to_gpx.py --input_csv "$input_csv" --output_gpx "${input_basename}.gpx"
+python3 utils/csv_to_gpx.py --input_csv "$input_csv" --output_gpx "${input_basename}.gpx" --date_time_gpx ${input_basename}
